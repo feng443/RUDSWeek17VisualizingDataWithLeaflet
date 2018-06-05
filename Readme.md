6b0f3478bbfe472c573414f1ad678375c666913c
Chan Feng <feng443@gmail.com>
2018-06-05 01:08:49 -0400

    Done all base requirements

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Unit 17 | Assignment - Visualizing Data with Leaflet
+
+
+
+To see this assignment in action click on URL below: 
+
+http://github.chanfeng.com/RUDSWeek17VisualizingDataWithLeaflet
+
+## Copyright
+
+Chan Feng (C) 2018. All Rights Reserved.``
--- logic.js
+++ logic.js
@@ -1,91 +1,78 @@
-// Define base maps
-var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoiZmVuZzQ0MyIsImEiOiJjamh2NDd3b2owdmNhM2tsNjA1dG0wcm8xIn0.XhlIawfI8vpUvUrSkpnIng" )
-var greyscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoiZmVuZzQ0MyIsImEiOiJjamh2NDd3b2owdmNhM2tsNjA1dG0wcm8xIn0.XhlIawfI8vpUvUrSkpnIng" )
-var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-  "access_token=pk.eyJ1IjoiZmVuZzQ0MyIsImEiOiJjamh2NDd3b2owdmNhM2tsNjA1dG0wcm8xIn0.XhlIawfI8vpUvUrSkpnIng" )
-// Define a baseMaps object to hold our base layers
-var baseMaps = {
+/*
+
+<Chan Feng> 2018-06-04 Rutgers Data Science Data Visualization Bootcamp
+
+*/
+
+// Store our API endpoint as earthquakeUrl
+const earthquakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+const tectonicUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json'
+
+const COLORS = [ '#B5F050', '#DDF051', '#ECD650', '#E9B24E', '#E59F66', '#E16364', ]
+const LABELS = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
+
+// Define base map layer
+const TOKEN = "access_token=pk.eyJ1IjoiZmVuZzQ0MyIsImEiOiJjamh2NDd3b2owdmNhM2tsNjA1dG0wcm8xIn0.XhlIawfI8vpUvUrSkpnIng"
+var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" + TOKEN)
+var greyscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" + TOKEN)
+var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" + TOKEN)
+ baseMaps = {
     'Satellite': satelliteMap,
     'Greyscale': greyscaleMap,
     'Outdoors': outdoorsMap,
 }
 
-// Store our API endpoint as queryUrl
-var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337"
-queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
-
-var tectonicUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json'
-
-const COLORS = [
-    '#B5F050',
-    '#DDF051',
-    '#ECD650',
-    '#E9B24E',
-    '#E59F66',
-    '#E16364',
-]
-
-var LABELS = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
-
-function getColor(mag) {
+// Dynamically create circle options
+function getCircleOptions(feature) {
+    mag = Math.abs(feature.properties.mag) // Occasionally mag is negative
+    i = Math.floor(mag)
     var i = i > 5 ? 5 : Math.floor(mag)
-    return COLORS[i]
-}
-
-function getTooltip(d) {
-    let place = d.properties.place
-    let mag = d.properties.mag
-    return `${place}: ${mag}`
-}
-
-var earthquakeMarkers = []
-d3.queue()
-.defer(d3.json, queryUrl)
-.defer(d3.json, tectonicUrl)
-.await(function(error, data, tectonicData) {
-  // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
-
-    data.features.forEach( d => {
-        let c = d.geometry.coordinates
-        let coord= [c[1], c[0]]
-        let mag = d.properties.mag
-        let place = d.properties.place
-        if (mag < 0 ) return;
-        let color = getColor(mag)
-        earthquakeMarkers.push(
-            L.circle(coord, {
-                radius: Math.sqrt(mag) * 40000,
-                color: color,
-                fillColor: color,
-                opacity: 0.8,
-            }).bindTooltip(getTooltip(d))
-
-        )
+    return {
+        radius: Math.sqrt(mag) * 10,
+        color: COLORS[i],
+        weight: 2,
+        fillColor: COLORS[i],
+        opacity: 0
     }
-    )
+}
+
+// Marker popup
+function getPopup(feature) {
+    let title = feature.properties.title
+    let detail = feature.properties.detail
+    return `<h2><a href=${detail} target=_new>${title}</a></h2>`
+}
+
+// Use d3.queue() to handle multiple data sources
+d3.queue()
+.defer(d3.json, earthquakeUrl)
+.defer(d3.json, tectonicUrl)
+.await(function(error, earthquakeData, tectonicData) {
+
+    // Earthquark Markers Layer
+    earthquakeLayer = L.geoJson(earthquakeData, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, getCircleOptions(feature))
+                    .bindPopup(getPopup(feature))
+        }
+    });
+
+    // Parse time automatically for time dimension control
+    earthquakeLayer = L.timeDimension.layer.geoJson(earthquakeLayer, {duration: 'PT4H'})
 
     // Add Legend
     var legend = L.control({ position: 'bottomright' })
     legend.onAdd = function (map) {
         var div = L.DomUtil.create('div', 'info legend')
         labels = []
-        // Add min & max
-        //div.innerHTML = '<div class="labels"><div class="min">' + LABELS[0] + '</div>' +
-		//	'<div class="max">' + LABELS[LABELS.length - 1] + '</div></div>'
-
         COLORS.forEach(function (color, i) {
-            labels.push('<li style="background-color: ' + color + '">' + LABELS[i] + '</li>')
+            labels.push('<li><span style="background-color: ' + color + '">&nbsp;&nbsp;&nbsp;&nbsp;</span> ' + LABELS[i] + '</li>')
         })
-
         div.innerHTML += '<ul>' + labels.join('') + '</ul>'
         return div
     }
 
-    earthquakeLayer = L.layerGroup(earthquakeMarkers)
-
+    // Fault Lines Layer
     var tectonicLayer = L.geoJson(tectonicData, {
         style: {
                 color: "orange",
@@ -101,9 +88,23 @@ d3.queue()
 
     // Create a new map
     var myMap = L.map("map", {
-        center: [ 37.09, -95.71 ],
-        zoom: 5,
-        layers: [satelliteMap, tectonicLayer, earthquakeLayer]
+        center: [ 37.09, -10 ], // Adjust center to show most of world map in a typical screen
+        zoom: 3,
+        layers: [satelliteMap, tectonicLayer, earthquakeLayer],
+        timeDimension: true,
+        timeDimensionControl: true,
+        timeDimensionOptions: {
+            period: 'PT1H'
+        },
+        timeDimensionControlOptions: {
+            autoPlay: true,
+            playerOptions: {
+                buffer: 10,
+                transitionTime: 200,
+                loop: true,
+        }
+    },
+
     })
 
     // Create a layer control containing our baseMaps
@@ -113,6 +114,4 @@ d3.queue()
     }).addTo(myMap)
 
     legend.addTo(myMap)
-
-
 })
